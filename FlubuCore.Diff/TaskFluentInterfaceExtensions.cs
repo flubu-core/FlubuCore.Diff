ddf073c1fcd745c58e90124aa2520d99b61a460e
markoz <mzorec@gmail.com>
2018-07-07 00:32:36 +0200

    Added diff task to task fluent interface.

---
+++ FlubuCore.Diff/TaskFluentInterfaceExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FlubuCore.Diff;
+
+namespace FlubuCore.Context.FluentInterface.Interfaces
+{
+    /// <summary>
+    /// Task compares 2 files and generates diff report in html.
+    /// </summary>
+    /// <param name="firstFilePathToCompare">path to the first file to compare</param>
+    /// <param name="secondFilePathToCompare">path to the second file to compare</param>
+    /// <param name="htmlReportOutputPath">path where html report will be stored.</param>
+    public static class TaskFluentInterfaceExtensions
+    {
+           public static DiffTask ExampleFlubuPluginTask(this ITaskFluentInterface flubu, string firstFilePathToCompare, string secondFilePathToCompare, string htmlReportOutputPath)
+        {
+            return new DiffTask(firstFilePathToCompare, secondFilePathToCompare, htmlReportOutputPath);
+        }
+    }
+}
